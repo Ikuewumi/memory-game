09c6f1ba082ea13b7eacf5a21dfe7a5f4f0406ec
Ikuewumi <ayobamsik@gmail.com>
2024-05-15 13:51:31 +0100

    content: +upper-rs-intro

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,15 +1,9 @@
-import path from 'path'
 import { describe, it, expect } from 'vitest'
 
 
 describe('Testing', _ => {
     it('Sum Testing', () => {
-
-        const d = path.resolve('./src/test')
-        console.log(d)
-
         expect(2 + 2).toBe(4)
-
     })
 
 })
